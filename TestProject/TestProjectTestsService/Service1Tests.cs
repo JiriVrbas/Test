e5849ccba2dca7908a0757757436e100977e0937
Jiri Vrbas <jiri0056@edu.easj.dk>
2017-09-25 10:32:21 +0200

    Create test

---
+++ TestProject/TestProjectTestsService/Service1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void SayMyNameTest()
+        {
+            TestProject.IService1 ser = new Service1();
+            var response = ser.SayMyName("Jiří");
+            var result = response.Contains("Jiří");
+            Assert.IsTrue(result);
+        }
+    }
+}
