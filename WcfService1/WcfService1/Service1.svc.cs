dea57b1f4198b644d0e79505e7844dd901c8cf71
Jiri Vrbas <jiri0056@edu.easj.dk>
2017-09-27 08:47:22 +0200

    CreateProjectStudents

---
+++ WcfService1/WcfService1/Service1.svc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private static List<Student> students = new List<Student>();
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public Student FindStudent(int id)
+        {
+            return students.Find(x => x.Id == id);
+        }
+
+        public bool RemoveStudent(int id)
+        {
+            return students.Remove(students.Find(x => x.Id == id));
+        }
+
+        public void EditStudent(Student student)
+        {
+            var st = students.Find(x => x.Id == student.Id);
+            if (st != null)
+            {
+                st.Name = student.Name;
+                st.Age = student.Age;
+                students.Remove(students.Find(x => x.Id == st.Id));
+                students.Add(st);
+            }
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ WcfService1/WcfService1TestsWcfService/Service1TestsStudents.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfService1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService1.Tests
+{
+    [TestClass()]
+    public class Service1TestsStudents
+    {
+        IService1 ser = new Service1();
+        [TestMethod()]
+        public void AddStudentTest()
+        {
+            int before = ser.GetAllStudents().Count;
+            ser.AddStudent(new Student()
+            {
+                Id = 1,
+                Name = "Jiří",
+                Age = 25
+            });
+            int now = ser.GetAllStudents().Count;
+            Assert.AreEqual(before + 1, now);
+        }
+        [TestMethod()]
+        public void FindStudent()
+        {
+            var student = new Student()
+            {
+                Id = 2,
+                Name = "Hanz",
+                Age = 37
+            };
+            ser.AddStudent(student);
+            Assert.IsNotNull(ser.FindStudent(2));
+        }
+        [TestMethod()]
+        public void RemoveStudent()
+        {
+            var student = new Student()
+            {
+                Id = 3,
+                Name = "Kirke",
+                Age = 32
+            };
+            ser.AddStudent(student);
+            ser.RemoveStudent(3);
+            Assert.IsNull(ser.FindStudent(3));
+        }
+        [TestMethod()]
+        public void EditStudent()
+        {
+            var student = new Student()
+            {
+                Id = 3,
+                Name = "Kirke",
+                Age = 32
+            };
+            ser.AddStudent(student);
+            ser.EditStudent(new Student()
+            {
+                Id = 3,
+                Name = "Karlos",
+                Age = 32
+            });
+            Assert.AreEqual(ser.FindStudent(3).Name,"Karlos");
+        }
+        [TestMethod()]
+        public void GetAllStudents()
+        {
+            Assert.IsNotNull(ser.GetAllStudents());
+        }
+    }
+}
